69f76a6a25fa789e42df9b8e876ebb45bd3bdfd7
pallavi.rbhate <pallavi.rbhate@gmail.com>
2020-11-04 18:28:26 +0530

    MultipleFileUploadErrorResolved

--- src/Components/Candidate/Profile/details/resume/Resume.jsx
+++ src/Components/Candidate/Profile/details/resume/Resume.jsx
@@ -55,39 +55,67 @@ class Resume extends Component {
       }
 
          //Delete Resume
-        deleteResume=()=>{            
+         deleteResume=()=>{   
+             
             // Calling Download Sample File Service from Service file:-
          this.fileService1.deleteSampleFile()
             .then(response => 
                     { 
-                        window.location.reload()
-             this.toast.show({severity: 'success', summary: 'Success Message', detail: 'User Deleted Successfully'},60000);
-            })
+                        localStorage.removeItem("SelectedFile")
+                        this.toast.show({severity: 'success', summary: 'Success Message', detail: 'User Deleted Successfully'},60000);
+                       
+                 })
              .catch(error=>{
-                    console.log("Error Occured...",error)})              
-                    
+                    console.log("Error Occured...",error)})    
+                               
+       
+              
+                   
         this.setState({    
                     deleteProductDialog: false                    
                     } )
+            
             }   
             
     //Delete Resume  
          state = { 
+            select:false,
+            drag:false,
             selectedFile: '',
             DraggedFile:'',
             productDialog: false,
             deleteProductDialog: false,
-            submitted: false
+            submitted: false,
+            showing:true
                  }; 
 
       onFileChange = event => {   
-        this.setState({ selectedFile: event.target.files[0]});  
+
+        if(this.state.drag==true)
+        {
+            this.toast.show({severity: 'error', summary: 'Error', detail: 'You have already dragged  a file'},50000);
+        }
+        else
+        {
+           this.setState({ selectedFile: event.target.files[0],
+            select:true});     
+        }
+    
       };  
 
  
-      onFileChange1 = (fileAccept) => {          
-        this.setState({ DraggedFile: fileAccept[0] });   
-        this.fileValidation()  
+      onFileChange1 = (fileAccept) => { 
+        if(this.state.select==true)
+        {
+            this.toast.show({severity: 'error', summary: 'Error', detail: 'You have already selected  a file'},50000);
+        }
+        else{
+        this.setState({ 
+            DraggedFile: fileAccept[0],
+            drag:true });   
+            this.fileValidation()  
+      };    
+         
               };    
 
         
@@ -111,7 +139,8 @@ class Resume extends Component {
         }
 
          //Dragging doc file to upload
-        uploadFile=()=> {
+        uploadFile=(e)=> {
+           e.preventDefault()
          
             if  (this.fileValidation())
             {                  
@@ -138,19 +167,31 @@ class Resume extends Component {
                             this.state.selectedFile,
                         ); 
             }
-           
+         
             // Calling Upload Sample File Service from Service file:-
             this.fileService1.postResumeFile(formData,formheader)
+         
             .then(Response=>{
-                        console.log(Response);
+               
+                        localStorage.setItem("SelectedFile",this.state.selectedFile.name || this.state.DraggedFile.name)
                         this.toast.show({severity: 'success', summary: 'Success Message', detail: 'File uploaded Successfully'},60000);
-                        //window.location.reload();
+                        window.location.reload()
+                       
                     },
                     )
 
             .catch(error=>{
                          this.toast.show({severity: 'error', summary: 'Error', detail: 'Server Error '},50000);})
                             }
+                            this.setState({  
+                                
+                                showing:false})
+                                
+                                this.setState({
+                                    select:false,
+                                    drag:false
+                                })
+                          
             }
 
          hideDialog() {
@@ -173,6 +214,7 @@ class Resume extends Component {
                 }
 
     render() {
+        const {showing}=this.state
         const deleteProductDialogFooter = (
             <>
             <Toast ref={(el) => this.toast = el} />
@@ -184,180 +226,88 @@ class Resume extends Component {
         return(
 
           <div class="bg-white px-4 py-4 section-divider align-items-center">
-
               <Toast ref={(el) => this.toast = el} />
-
                    <div class="col">
-
                      <div class="mb-4 align-items-center">
-
                        <img src="/images/Dashboard-assets/resume-icon.svg" alt="Cinque Terre" class="mr-2" />
-
                        <span class="subtitle-semi-bold">Upload Resume</span>
-
                      </div>
-
                    </div>
-
                    <div class="col-12 mb-3">
-
                      <img src="/images/Dashboard-assets/candidate/push-chevron-down-o.png"   alt="Cinque Terre" class="ml-4 mr-2 left-sec-icon" onClick={this.downloadResume} />
-
-                     <span class="mr-3" style={{ color: '#007EFF' }}>{this.state.selectedFile.name}</span>
-                     <span class="mr-3" style={{ color: '#007EFF' }}>{this.state.DraggedFile.name}</span>
-
+                     <span class="mr-3" style={{ color: '#007EFF' }}>{localStorage.getItem("SelectedFile")}</span>
+                     <span class="mr-3" style={{ color: '#007EFF' }}>{localStorage.getItem("DraggedFile")}</span>
                      <span>Last updated on 10 sept 2020</span>
-
-                     <a className="download_sample_link d-block  text-right" href="#" onClick={this.confirmDeleteProduct}>Delete Resume</a>
-
+                     <a className="download_sample_link d-block text-right" href="#" onClick={this.confirmDeleteProduct}>Delete Resume</a>
                    </div>
-
                    <div class="col">
-
                    <section className="content_section">
-
                              <div className="ml-0 mr-1">
-
                                  <section className="white-middle-section mt-5">
-
                                     <div className="row">
-
-                                         {/* CSV file upload */}
-
-                                       
-
+                                         {/*  file upload */}
                                          <div className="col-md-6 offset-md-3 p-4">
-
-                                            
-
                                              <div className="text-center mt-5">
-
                                              <Dropzone
-
-                                             onDrop={this.onFileChange1}
-
-                                            // accept=".csv"
-
-                                            >
-
+                                             onDrop={this.onFileChange1} 
+                                           >
                                             {({getRootProps, getInputProps, isDragActive, isDragReject, rejectedFiles}) => {
-
                                             return (
-
                                                 <div {...getRootProps({className:"dropzone"})}>
-
                                                     <img src="/images/Dashboard-assets/cloud-upload.svg" alt="cloud upload" className="cloud_upload_logo pb-2" />
-
                                                 <input {...getInputProps() }   />
-
-                                                
-
                                                 {!isDragActive && 'Click here or drop a file to upload!'}
-
+                                                {showing ?
                                                 <div className="file-path-wrapper font-blue">
-
                                                         <input className="file-path validate" type="text" value={this.state.DraggedFile.name} placeholder=""/> 
-
-                                                        
-
-                                                </div>
-
-                                                
-
+                                                </div>:null}
                                                 {isDragActive && !isDragReject && "Drop it like it's hot!"}
-
                                                 {isDragReject && "File type not accepted, sorry!"}
-
-                                               
-
                                                 </div>
-
                                             )}
-
                                             }
-
                                             </Dropzone>
-
-                                            
-
                                                  </div>
-
                                             <p className="text-center">or</p>
-
                                             <form action="">
-
                                                 <div className="text-center d-flex justify-content-center">
-
                                                 <div className="file-field d-flex-inline">
-
                                                     <div className="btn btn-blue btn-sm float-left waves-effect waves-light">
-
                                                         <span>Choose file</span>
-
                                                         <input type="file" id="myFile" name="filename" accept="application/pdf,application/msword,  application/vnd.openxmlformats-officedocument.wordprocessingml.document"  onChange={this.onFileChange} />
-
-                                                    
-
                                                     </div>
-
+                                                    <div>
+               
+                                                    </div>  
+                                                     {/* { showing ? */}
+                 
                                                     <div className="file-path-wrapper">
-
-                                                        <input className="file-path validate" type="text" value={this.state.selectedFile.name} placeholder="No file choosen"/> 
-
-                                                    </div>
-
+                                                        <input className="file-path validate" type="text" value={this.state.selectedFile.name} placeholder="No file choosen"/>
+                                                        {console.log(showing)} 
+                                                       
+                                                     </div>
+                                                   {/* /* : null */} 
                                                 </div>
-
-                                                                        
-
                                             </div>
-
                                             </form>
-
                                             <p className="text-center mt-4"></p>
-
-                                           
-
                                         </div>
-
-                                        
-
                                     </div>    
-
                                 </section>
-
                                 <div className="ml-2 mt-4">
-
                                     <button type="button" className="btn btn-blue" onClick={this.uploadFile}>Upload</button>
-
                                 </div>                                   
-
                     <Dialog visible={this.state.deleteProductDialog} style={{ width: '450px' }} header="Confirm" modal footer={deleteProductDialogFooter} onHide={this.hideDeleteProductDialog}>
-
                     <div className="confirmation-content">
-
                         <i className="pi pi-exclamation-triangle p-mr-3" style={{ fontSize: '2rem'}} />
-
                          <span>Are you sure you want to delete</span>
-
                     </div>
-
                 </Dialog>
-
                             </div>
-
                         </section>
-
                    </div>
-
                  </div>
-
         );
-
     }
-
 }
-
- 
-
 export default Resume; 
