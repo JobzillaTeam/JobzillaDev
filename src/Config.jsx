109a815b4840b7cef43d6f8d84baeff818c27e79
Prashant Darshan <pd00558235@techmahindra.com>
2020-11-19 17:06:47 +0530

    commi for Base url for new instance for tetsing

--- src/Config.jsx
+++ src/Config.jsx
@@ -8,5 +8,9 @@ export const ApiHeader = {headers: { 'Content-Type': 'application/json'}}
 //Base URL to use in AWS- Ohio region
 //export const ApiBaseUrl = "http://jobzilla-env.eba-p2gnwpc9.us-east-2.elasticbeanstalk.com/jobs";
 
+
+//Base URL to use for Testing AWS Instance
+// export const ApiBaseUrl = "http://iswitchapiuat-env.eba-xwdztrgc.ap-south-1.elasticbeanstalk.com/jobs";
+
 //Base URL to use in AWS- Mumbai region
 export const ApiBaseUrl = "http://iswitchapp-env.eba-9u3aunju.ap-south-1.elasticbeanstalk.com/jobs";
