766061a5a25444d2c6120f1ed488f6f47e770174
Prashant Darshan <pd00558235@techmahindra.com>
2020-12-16 02:16:35 +0530

    commit

--- src/Components/CommonComp/HeaderAll.jsx
+++ src/Components/CommonComp/HeaderAll.jsx
@@ -11,7 +11,7 @@ class HeaderAll extends Component {
       userData: [],
       userId: localStorage.getItem('candidateId'),
       status: '',
-      imageUrl:''
+      imageUrl: ''
     }
     this.toggleHandeler = this.toggleHandeler.bind(this)
     this.viewImage = new ApiServicesOrg()
@@ -24,24 +24,27 @@ class HeaderAll extends Component {
     const providerRecruiterStatus = status;
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.viewImage.viewProfileImage()
-    .then(Response => {
+      .then(Response => {
         //console.log(Response.data.responseObject)
-        this.setState({
+        if (Response && Response.data && Response.data.responseObject) {
+          this.setState({
             imageUrl: Response.data.responseObject
-        })
-         })
-}
+          })
+          // localStorage.setItem('imageUrl', `${Response.data.responseObject}`)
+        }
+      })
+  }
 
 
   render() {
     console.log('Render Header')
     const { isCandidate } = this.props;
     const { isSetting } = this.props;
-    const {isProfile} = this.props;
+    const { isProfile } = this.props;
     const { status } = this.state
-    const userName = '';
+    const userName = localStorage.getItem('userName');
     const providerRecruiterStatus = localStorage.getItem('status')
     if (localStorage.userRole === "candidate_role") {
       if (this.context.state instanceof Promise) {
@@ -49,10 +52,18 @@ class HeaderAll extends Component {
           if (data) {
             const imageUrlPath = data.userImage ? `data:image/jpeg;base64,${data.userImage}` : '/images/Dashboard-assets/user-f.png'
             const firstNameValue = data.candidateInfo && data.candidateInfo.firstName ? data.candidateInfo.firstName : '';
-            const lastNameValue = data.candidateInfo && data.candidateInfo.lastName ? data.candidateInfo.lastName : ''; 
+            const lastNameValue = data.candidateInfo && data.candidateInfo.lastName ? data.candidateInfo.lastName : '';
             const userNameValue = (firstNameValue || lastNameValue) ? `${firstNameValue} ${lastNameValue}` : localStorage.getItem('userName')
-            document.getElementById("_userImageAvatarIcon").src = imageUrlPath
-            document.getElementById("_currentUserName").innerHTML = userNameValue
+            const imageElement = document.getElementById("_userImageAvatarIcon")
+            const innerHtmlElement = document.getElementById("_currentUserName")
+            const imageSrc = imageElement.src;
+            const innerHtmlText = innerHtmlElement.innerHTML
+            if (imageSrc !== imageUrlPath) {
+              imageElement.src = imageUrlPath
+            }
+            if (innerHtmlText !== userNameValue) {
+              innerHtmlElement.innerHTML = userNameValue
+            }
           }
         })
       }
@@ -60,10 +71,14 @@ class HeaderAll extends Component {
       new ApiServicesOrg().getOrganizationProfile().then((response) => {
         const responseObject = response && response.data && response.data.responseObject;
         const userNameValue = responseObject && responseObject.contactPerson ? responseObject.contactPerson : localStorage.getItem('userName');
-        document.getElementById("_currentUserName").innerHTML = userNameValue;
+        const innerHtmlElement = document.getElementById("_currentUserName")
+        const innerHtmlText = innerHtmlElement.innerHTML
+        if (innerHtmlText !== userNameValue) {
+          innerHtmlElement.innerHTML = userNameValue;
+        }
       });
     }
-    
+
     return (
       <div>
         {this.state.status === "provider" && <Redirect to="/providerDashboard" />}
@@ -77,18 +92,18 @@ class HeaderAll extends Component {
             <div className="float-left d-inline-flex marL34">
               <div className="mx-3 sub-title1 d-flex align-items-center">JOB : </div>
               <div className="btn-group btn-group-toggle my-auto" data-toggle="buttons">
-              <label className={`btn btn-toggler ${providerRecruiterStatus === "provider" ? " active" : " "}`}>                  <input type="radio"
-                    value="provider"
-                    checked={providerRecruiterStatus  === "provider"}
-                    autoComplete="off"
-                    onClick={(e) => this.toggleHandeler("provider")}
-                    defaultChecked="provider"
-                  /> Provider
+                <label className={`btn btn-toggler ${providerRecruiterStatus === "provider" ? " active" : " "}`}>                  <input type="radio"
+                  value="provider"
+                  checked={providerRecruiterStatus === "provider"}
+                  autoComplete="off"
+                  onClick={(e) => this.toggleHandeler("provider")}
+                  defaultChecked="provider"
+                /> Provider
             </label>
-            <label className={`btn btn-toggler ${providerRecruiterStatus === "recruiter" ? "active" : " "}`}>
-                 <input type="radio"
+                <label className={`btn btn-toggler ${providerRecruiterStatus === "recruiter" ? "active" : " "}`}>
+                  <input type="radio"
                     value="recruiter"
-                    checked={providerRecruiterStatus  === "recruiter"}
+                    checked={providerRecruiterStatus === "recruiter"}
                     autoComplete="off"
                     onClick={(e) => this.toggleHandeler("recruiter")}
                   /> Recruiter
@@ -97,27 +112,27 @@ class HeaderAll extends Component {
               </div>
             </div> : null}
           <ul className="nav mr-3">
-          <li>
-            {this.state.imageUrl ? <img id="_userImageAvatarIcon" className="rounded-circle profile-icon mr-2" src={`data:image/jpeg;base64,${this.state.imageUrl}`} width="35" height="35"/>
-            : <img id="_userImageAvatarIcon" className="rounded-circle profile-icon mr-2" src="/images/Dashboard-assets/user-f.png"width="35" height="35" alt="User profile"/>}
+            <li>
+              {this.state.imageUrl ? <img id="_userImageAvatarIcon" className="rounded-circle profile-icon mr-2" src={`data:image/jpeg;base64,${this.state.imageUrl}`} width="35" height="35" />
+                : <img id="_userImageAvatarIcon" className="rounded-circle profile-icon mr-2" src="/images/Dashboard-assets/user-f.png" width="35" height="35" alt="User profile" />}
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#" data-toggle="dropdown">
                 <span className="font-blue text-small marL10" ><span id="_currentUserName">{userName}</span><i className="fa fa-angle-down pl-2" aria-hidden="true"></i></span>
               </a>
               <ul className="dropdown-menu">
-              {(!isCandidate)?
-                <li>
-                  <Link className="dropdown-item" to="/orgProfile" >
-                    {/* <i className="fa fa-user pr-2" aria-hidden="true"></i>*/} Profile 
+                {(!isCandidate) ?
+                  <li>
+                    <Link className="dropdown-item" to="/orgProfile" >
+                      {/* <i className="fa fa-user pr-2" aria-hidden="true"></i>*/} Profile
                     </Link>
-                </li>
-                :
-                <li>
-                <Link className="dropdown-item" to="/candidate/changePassword" >
-                  {/* <i className="fa fa-user pr-2" aria-hidden="true"></i>*/} Change Password 
+                  </li>
+                  :
+                  <li>
+                    <Link className="dropdown-item" to="/candidate/changePassword" >
+                      {/* <i className="fa fa-user pr-2" aria-hidden="true"></i>*/} Change Password
                   </Link>
-                </li>}
+                  </li>}
 
                 {/* If candidate is logged in then candidate Email settings will open otherwise provider/Recruiter Email Settings will open */}
                 <li>
